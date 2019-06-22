7ea9aed15e07ad238cddc7ad7efcdb285d9e01a0
Danny Lee <dreamer@panix.com>
2019-06-22 02:57:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,7 @@
 # code your #valid_move? method here
+def valid_move? (board, move)
 
+<<<<<<< HEAD
 def valid_move?(board, index)
   if (index.between?(0,8) && !position_taken?(board,index))
     return true
@@ -17,6 +19,22 @@ def position_taken?(board, index)
   end
   if (board[index] == " " || board[index] == "" || board[index] == NIL)
     return false
+=======
+  if move > board.length + 1
+    return FALSE
+  else
+    return TRUE
+end   
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken? (board, index)
+  if (board[index] == "X" || (board[index]) == "O") 
+    return TRUE
+  end
+  if (board[index] == " " || board[index] == "" || board[index] == NIL)
+    return FALSE
+>>>>>>> fdbd411353c462a5a01c19743390b579373e5496
   end
 
 end
